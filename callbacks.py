65876605430043ff5d27b5e094d41ac7433a5c3c
MaiaMCole <maia_cole@icloud.com>
2023-10-14 11:24:45 +0200

    initial commit

---
+++ callbacks.py
@@ -0,0 +1,8 @@
+import typer
+
+
+def get_title_from_body(body: str) -> str:
+    """
+    In the case that there is no title given a title is created from the first words of the the new note's body.
+    """
+    pass
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Get Started
+
+1. Create a python virtual env named IAVenv so that it matches the .gitignore file.
+    - `python -m venv IAVenv`
+2. Install the dependencies.
+    - `pip install -r requirements.txt`
